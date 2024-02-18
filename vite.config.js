6e975a88cc3c2890c9a402d2c63b8c1594846ff7
ravi-devo <ravi.ravichandran370@gmail.com>
2024-02-18 22:38:13 +0530

    Proxy update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], proxy: {
-    '/api': {
-      target: 'https://faceoftamizhan-backend-ravi-devo.onrender.com',
-      changeOrigin: true,
+  plugins: [react()], 
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://faceoftamizhan-backend-ravi-devo.onrender.com',
+        changeOrigin: true,
+      },
     },
   },
 })
